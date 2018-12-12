8ce34ee11db3ba1749577e5372b86288767a9da4
Raphael Michel <mail@raphaelmichel.de>
2018-12-12 16:13:25 +0100

    Add warning

--- pretix_zugferd/__init__.py
+++ pretix_zugferd/__init__.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext, ugettext_lazy
 
 
 class PluginApp(AppConfig):
@@ -9,9 +10,22 @@ class PluginApp(AppConfig):
     class PretixPluginMeta:
         name = ugettext_lazy('ZUGFeRD invoices for pretix')
         author = 'Raphael Michel'
-        description = ugettext_lazy('Invoice renderer that annotates pretix invoices with ZUGFeRD data')
         visible = True
         version = '1.0.0'
+
+        @property
+        def description(self):
+            t = ugettext('This plugin provides an invoice renderer that annotates pretix invoices with ZUGFeRD data, '
+                         'a structured data format for invoices used in Germany.')
+            t += '<div class="alert alert-legal">'
+            t += ugettext(
+                'Note: Use this plugin at your own risk. If there is a semantic difference between the XML and PDF '
+                'contents in your ZUGFeRD invoices, you might legally owe the VAT to the financial authorities twice, '
+                'since you then legally sent two invoices. We tried our best to avoid this, but we do not assume '
+                'any liability. Please check the output of this plugin with your tax or legal attorney before use.'
+            )
+            t += '</div>'
+            return mark_safe(t)
 
     def ready(self):
         from . import signals  # NOQA
