b00b54d0d0b8365298f311c946bf0643ce75cb98
Raphael Michel <mail@raphaelmichel.de>
2020-12-22 12:43:40 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {
 
 setup(
     name='pretix-zugferd',
-    version='1.0.3',
+    version='1.0.4',
     description='Invoice renderer that annotates pretix invoices with ZUGFeRD data',
     long_description=long_description,
     url='https://github.com/pretixeu/pretix-zugferd',
