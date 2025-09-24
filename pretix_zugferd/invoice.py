f7c353f2e4018a5dae87a9d08051d87a08ce41e0
Raphael Michel <michel@rami.io>
2025-09-24 09:40:07 +0200

    Move person name and email into other fields (Z#23207300) (#17)
    * Move person name and email into other fields (Z#23207300)
    
    * Run black

--- pretix_zugferd/invoice.py
+++ pretix_zugferd/invoice.py
@@ -207,16 +207,12 @@ class ZugferdMixin:
         doc.trade.agreement.buyer.address.postcode = remove_control_characters(
             invoice.invoice_to_zipcode
         )
-        if invoice.invoice_to_company and invoice.invoice_to_name:
-            doc.trade.agreement.buyer.address.line_one = remove_control_characters(
+        doc.trade.agreement.buyer.address.line_one = remove_control_characters(
+            invoice.invoice_to_street
+        )
+        if invoice.invoice_to_name:
+            doc.trade.agreement.buyer.contact.person_name = remove_control_characters(
                 invoice.invoice_to_name
-            )
-            doc.trade.agreement.buyer.address.line_two = remove_control_characters(
-                invoice.invoice_to_street
-            )
-        else:
-            doc.trade.agreement.buyer.address.line_one = remove_control_characters(
-                invoice.invoice_to_street
             )
         doc.trade.agreement.buyer.address.city_name = remove_control_characters(
             invoice.invoice_to_city
@@ -241,6 +237,8 @@ class ZugferdMixin:
                 "EM",
                 invoice.order.email,
             )
+        if invoice.order.email:
+            doc.trade.agreement.buyer.contact.email.address = invoice.order.email
 
         if invoice.invoice_to_vat_id:
             doc.trade.agreement.buyer.tax_registrations.add(
@@ -379,7 +377,9 @@ class ZugferdMixin:
             doc.trade.settlement.invoice_referenced_document.issuer_assigned_id = (
                 invoice.refers.number
             )
-            doc.trade.settlement.invoice_referenced_document.issue_date_time = invoice.refers.date
+            doc.trade.settlement.invoice_referenced_document.issue_date_time = (
+                invoice.refers.date
+            )
 
         taxtotal = Decimal(0)
         for idx, gross in grossvalue_map.items():
